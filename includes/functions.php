d9e2d46e4571f041f82596beb948f5113e28a766
Nguyen_Anh <panhsl14102004@gmail.com>
2024-04-11 09:15:17 +0700

    Commit website

--- includes/functions.php
+++ includes/functions.php
@@ -280,13 +280,13 @@ function sendMail($to, $subject, $content) {
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'panhtc141004@gmail.com';                     //SMTP username
-        $mail->Password   = 'pfcpgxctdmwnmhvc';                               //SMTP password
+        $mail->Username   = 'test@gmail.com';                     //SMTP username
+        $mail->Password   = 'testToken###';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('AnhNg@gmail.com', 'Mailer');
+        $mail->setFrom('test@gmail.com', 'Mailer');
         $mail->addAddress($to);     //Add a recipient
 
         //Content
